c417b33265b9bbc929b5cd1a09ffdee40c178cb3
Ashima Shrivastava <ashima206@gmail.com>
2016-10-18 16:03:05 -0400

    added select bar

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,22 @@ var processData = function (object) {
 		var typeName  = reqdField.type;
 		var iconName =reqdField.icon;
 		var idName = reqdField.id;
-		$(".input-blocks").append(`<div class ="big-box"><input class="box" type="${typeName}" id="${idName}" placeholder="${labelName}">
-									<i id="image" class="fa ${iconName}"  aria-hidden="true"></i></div>`);
+		if(typeName=="select"){
+			var $newSelect = $(`<select name="select" class="dropDownBox"><option selected disabled> Select Language</option> </select>`);//how did you define newSelect variale
+			$(".input-blocks").append($newSelect);
+			for(count=0;count<reqdField.options.length;count++){
+				var optionName = reqdField.options[count]; 
+				$newSelect.append(`<option value ="${optionName.label}">${optionName.label}</option>`);
+			}
+
+		} else {
+			$(".input-blocks").append(`<div class ="big-box"><input class="box" type="${typeName}" id="${idName}" placeholder="${labelName}">
+									<i  id ="image"class="fa ${iconName}" aria-hidden="true"></i></div>`);
 		}
 		
 	}
-
+}
+	
 var getData = function(event){
 	var reqdData = $.ajax({
 		url: "http://json-data.herokuapp.com/forms",
